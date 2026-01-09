4b9a65ec6a904cd5c7ca1868d8e0792317039201
backspace <anh.phan156@protonmail.com>
2026-01-09 14:58:54 -0500

    flake update

--- flake.nix
+++ flake.nix
@@ -3,63 +3,12 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    self.submodules = true;
   };
 
   outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
   in {
-    packages.${system}.default = pkgs.callPackage ({
-      stdenv,
-      pkg-config,
-      meson,
-      ninja,
-      cmake,
-      wayland-protocols,
-      wayland-scanner,
-      wayland,
-      cairo,
-      libxkbcommon,
-      cjson,
-      imagemagick,
-      scdoc,
-      makeWrapper,
-      lib,
-      grim,
-    }:
-      stdenv.mkDerivation {
-        name = "mscreenshot";
-        src = ./.;
-
-        depsBuildBuild = [pkg-config];
-        strictDeps = true;
-
-        nativeBuildInputs = [
-          meson
-          ninja
-          pkg-config
-          cmake
-          scdoc
-          makeWrapper
-        ];
-
-        buildInputs = [
-          wayland-protocols
-          wayland-scanner
-          wayland
-          cairo
-          libxkbcommon
-          cjson
-          imagemagick.dev
-        ];
-
-        postInstall = ''
-          wrapProgram $out/bin/mscreenshot \
-          --suffix PATH : ${lib.makeBinPath [grim]}
-        '';
-      }) {};
-
     devShells.${system}.default = pkgs.mkShell {
       nativeBuildInputs = with pkgs; [
         meson
